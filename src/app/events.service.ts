a9ff7e9d70253f206dc3c94827495b66dfe66b46
fernandoNico <nicolalde3006@gmail.com>
2018-07-11 19:52:32 +0100

    File formatted and commented
    The code has been formatted and commented properly with meaningful comments

--- src/app/events.service.ts
+++ src/app/events.service.ts
@@ -2,40 +2,38 @@ import { Injectable } from '@angular/core';
 import { Http, HttpModule, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http';
 import { Observable  } from 'rxjs/Observable';
+import { Event} from './ui/landing-page/event.model'; // Event Class used to encapsulate data 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/Observable/throw';
 import 'rxjs/add/operator/toPromise';
 
-import { Event} from './ui/landing-page/event.model';
-
-
 @Injectable()
 export class EventsService {
   
-  apiKey = 'OQMi5v8uiECOE_y_RIyHOg11426';
-    url = 'http://localhost:56647/api/Events';
-    options: RequestOptions;
-   
-  
-    constructor(private http: Http) {}
-  
-    getEvents() {
-      return this.http.get(('http://localhost:56647/api/Events'))
-      .map((res: Response) => res.json()).catch(this.handleError);
-    }
-  
-    getEventById(id: number): Observable<Event> {
-      return this.http.get('http://localhost:56647/api/Events/' + id )
-      .map((response: Response) => <Event>response.json()).catch(this.handleError);
-    }
-  
-    getEventPostcode(postcode: string) {
-      return this.http.get('https://api.getAddress.io/find/' + postcode + '?api-key=' + this.apiKey)
-      .map((res: Response) => res.json()).catch(this.handleError);
-    }
-  
-  addEventos(event:Event){
+  apiKey = 'OQMi5v8uiECOE_y_RIyHOg11426'; // APi Key to fecth data from the postcode API
+  url = 'http://localhost:56647/api/Events'; //Restful Web API endpoint
+  options: RequestOptions;
+
+  constructor(private http: Http) {}
+
+  //Fetch all events stored in the relational DB
+  getEvents() {
+    return this.http.get(('http://localhost:56647/api/Events'))
+    .map((res: Response) => res.json()).catch(this.handleError);
+  }
+  //Fetch an event based on its id
+  getEventById(id: number): Observable<Event> {
+    return this.http.get('http://localhost:56647/api/Events/' + id )
+    .map((response: Response) => <Event>response.json()).catch(this.handleError);
+  }
+  //Fetch an event location based on the event postcode
+  getEventPostcode(postcode: string) {
+    return this.http.get('https://api.getAddress.io/find/' + postcode + '?api-key=' + this.apiKey)
+    .map((res: Response) => res.json()).catch(this.handleError);
+  }
+  //Push an event to the relational DB
+  addEvents(event:Event){
       let endpoint = this.url;
       let body = JSON.stringify(event);
       let headers = new Headers({'Content-Type':'application/json'});
@@ -44,10 +42,9 @@ export class EventsService {
           .map((res:Response) => res.json())
           .catch((error:any) => Observable.throw(error.json().error || 'Server Error'));
   }
-  
+  //Update an event to the relational DB
   updateEvent(event:Event, id:number){
     let endpoint = `${this.url}/${id}`;
-    //let endpoint = this.url + event.EventId;
     console.log(endpoint);
     let body = JSON.stringify(event);
     let headers = new Headers({'Content-Type':'application/json'});
@@ -56,18 +53,13 @@ export class EventsService {
         .map((res:Response) => res.json())
         .catch((error:any) => Observable.throw(error.json().error || 'Server Error'));
   }
-  
-  
-  
-  
+   //Delete an event to the relational DB 
   deleteEvent(id:number) {
-    return this.http.delete('http://localhost:56647/api/Events/' +  id, this.options).map((response: Response) => response.json())
+    return this.http.delete('http://localhost:56647/api/Events/' +  id, this.options).
+    map((response: Response) => response.json())
     .catch(this.handleError);
   }
-  
-  
-  
-    //////////
+  // Handle error method 
     handleError(error: Response) {
       console.error(error);
       return Observable.throw(error);
